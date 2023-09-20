e89bd8a51b9cefb2fa810984f055e849da8f688e
bruno-bahr <brunobahrbr@gmail.com>
2023-09-20 16:08:44 -0300

    Slugify insertion

--- src/main/java/br/com/brunobahr/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/brunobahr/placeservice/domain/PlaceService.java
@@ -1,18 +1,22 @@
 package br.com.brunobahr.placeservice.domain;
 
 import br.com.brunobahr.placeservice.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
 
     private PlaceRepository placeRepository;
 
+    private Slugify slg;
+
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(),
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(),
                 placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
